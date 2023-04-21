386f5df4f8256517790cc9a723d284dbb64cbb7a
enhoker <45732134+enhoker@users.noreply.github.com>
2023-04-21 21:12:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 689985882 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    a = False
+    ttest = ttest_ind(x, y, equal_var=False)
+    p = ttest.pvalue
+    if (p<=0.01):
+        a = True
+    return a
