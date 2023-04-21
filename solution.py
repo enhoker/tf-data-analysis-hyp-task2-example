f8a0a0111dd183ce90efcb850df5e7e01b5d73ee
enhoker <45732134+enhoker@users.noreply.github.com>
2023-04-21 21:19:47 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,9 +6,9 @@ from scipy.stats import ttest_ind
 chat_id = 689985882 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    a = False
+    a = True
     ttest = ttest_ind(x, y, equal_var=False)
     p = ttest.pvalue
     if (p<=0.01):
-        a = True
+        a = False
     return a
